945b0c9e527cc382c4ea9574d13a529292560bf6
chris-bungay <chris.bungay@planninginspectorate.gov.uk>
2023-05-15 17:59:09 +0100

    Re-instated redaction status change on reps summary screen

--- app/views/banners-1418/routes/routes.js
+++ app/views/banners-1418/routes/routes.js
@@ -84,7 +84,7 @@ router.post("/banners-1418/load-prototype-data", function(req, res) {
         "content": "The application is expected to be re-submitted to the Planning Inspectorate in Autumn 2022.",
         "emailSubscribers": "Yes",
         "author": "Joe Bloggs",
-        "status": "Archived",
+        "status": "Draft",
         "bannerColourClass": "govuk-tag--red"
       },
       {
@@ -93,17 +93,17 @@ router.post("/banners-1418/load-prototype-data", function(req, res) {
         "content": "The application has been withdrawn. Please see the Applicant’s letter (PDF, 119KB).",
         "emailSubscribers": "Yes",
         "author": "Joe Bloggs",
-        "status": "Archived",
+        "status": "Published",
         "bannerColourClass": "govuk-tag--red"
       }
     ]
     console.log (req.session.data['banners']);
       res.redirect("/banners-1418/");
-  
+
   });
-  
+
   router.post("/banners-1418/load-prototype-data-welsh", function(req, res) {
-  
+
     req.session.data['welsh']= true;
     console.log(req.session.data['welsh']);
     req.session.data['banner'];
@@ -204,7 +204,7 @@ router.post("/banners-1418/load-prototype-data", function(req, res) {
         "contentWelsh": "Dyma enghraifft o destun baner yn Gymraeg a fyddai'n mynd i gynnwys y faner",
         "emailSubscribersWelsh":"Yes",
         "author": "Joe Bloggs",
-        "status": "Archived",
+        "status": "Published",
         "bannerColourClass": "govuk-tag--red"
       },
       {
@@ -215,147 +215,147 @@ router.post("/banners-1418/load-prototype-data", function(req, res) {
         "contentWelsh": "Dyma enghraifft o destun baner yn Gymraeg a fyddai'n mynd i gynnwys y faner",
         "emailSubscribersWelsh":"Yes",
         "author": "Joe Bloggs",
-        "status": "Archived",
+        "status": "Published",
         "bannerColourClass": "govuk-tag--red"
       }
     ]
     console.log (req.session.data['banners']);
       res.redirect("/banners-1418/");
-  
+
   });
-  
+
   router.post("/banners-1418/change-banner-form-answer", function(req, res) {
-  
+
     if( req.session.data['change-banner-position'] ){
-  
+
     let choice = req.session.data['change-banner-position']
     console.log(choice)
     req.session.data['banner'] = req.session.data['banners'][choice];
     console.log(req.session.data['banner']);
     }
-  
-  
+
+
       res.redirect("/banners-1418/summary");
-  
-  
+
+
   });
-  
+
   router.post("/banners-1418/create-journey-start", function(req, res) {
-  
+
     if( req.session.data['welsh'] ){
       res.redirect("/banners-1418/create/welsh/index");
-  
-  
+
+
     }
   else {
-  
+
   }    res.redirect("/banners-1418/create/index");
-  
-  
-  
-  
+
+
+
+
   });
-  
+
   router.post("/banners-1418/change-status-routing", function(req, res) {
     //Colour logic and IP number
     if (req.session.data.banner['status'] == "Draft"){
       req.session.data.banner['bannerColourClass'] = "govuk-tag--grey";
     }
-  
+
     else if (req.session.data.banner['status'] == "Published"){
       req.session.data.banner['bannerColourClass'] = "govuk-tag--blue";
     }
-  
+
     else {
       req.session.data.banner['bannerColourClass'] = "govuk-tag--red";
     }
-  
+
         res.redirect("summary");
     //IP number
-  
+
     });
-  
+
     router.post("/banners-1418/set-status-routing", function(req, res) {
       //Colour logic and IP number
       if (req.session.data.banner['status'] == "Draft"){
         req.session.data.banner['bannerColourClass'] = "govuk-tag--grey";
       }
-  
+
       else if (req.session.data.banner['status'] == "Published"){
         req.session.data.banner['bannerColourClass'] = "govuk-tag--blue";
       }
-  
+
       else {
         req.session.data.banner['bannerColourClass'] = "govuk-tag--red";
       }
-  
+
           res.redirect("create/check-answers");
       //IP number
-  
+
       });
-  
+
     router.post("/banners-1418/summary-routing", function(req, res) {
-  
+
       if(!req.session.data['banners']) {
       req.session.data['banners'] = []
       }
-  
+
       // set corrections array as a variable
       let submissionData = req.session.data['banners']
-  
+
       // access the set of sales details the user has just entered
       let choice = req.session.data['banner']
-  
-  
+
+
       // check if the user is changing some details already entered
       let change = req.session.data['change-banner-position']
-  
+
       // call the function to add the latest correction to the corrections
       addToList(choice, submissionData, change)
-  
-  
+
+
               delete req.session.data['change-banner-position']
-  
-  
+
+
       console.log(req.session.data['banners'])
       res.redirect("index");
-  
+
   });
-  
-  
+
+
   router.post("/banners-1418/check-answers-routing", function(req, res) {
-  
+
     if(!req.session.data['banners']) {
     req.session.data['banners'] = []
     }
 
     const sessionData = req.session.data
-  
+
     //Set banner date
     req.session.data.banner['dateCreated'] = "1 March 2023";
     req.session.data.banner['author'] = "Bob Bloggs";
     req.session.data['bannerAlert'] = true;
-  
+
     // set corrections array as a variable
     let submissionData = req.session.data['banners']
-  
+
     // access the set of sales details the user has just entered
     let choice = req.session.data['banner']
-  
-  
+
+
     // check if the user is changing some details already entered
     let change = req.session.data['change-banner-position']
-  
+
     // call the function to add the latest correction to the corrections
     addToList(choice, submissionData, change)
-  
-  
+
+
             delete req.session.data['change-banner-position']
-  
-  
+
+
     console.log(req.session.data['banners'])
     res.redirect("/banners-1418/index");
-  
+
   });
-  
+
   module.exports = router
